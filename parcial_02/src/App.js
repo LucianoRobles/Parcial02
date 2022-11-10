45f6e63404e078a194b76767c07b06544d755631
Luciano Robles <54420250+LucianoRobles@users.noreply.github.com>
2022-11-10 20:45:26 -0300

    Comenzando 01

--- parcial_02/src/App.js
+++ parcial_02/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
+import { Fragment } from 'react';
 import './App.css';
+import Footer from './componentes/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container,Row} from '../node_modules/react-bootstrap/';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Container>
+          <Row>
+            <h1>Aca va el header</h1>
+          </Row>
+          <Row>
+            <h1>Aca va el body</h1>
+          </Row>
+          <Row>
+            <Footer/>
+          </Row>
+        </Container>
+          
+          
+          
+
+    </Fragment>
   );
 }
 
